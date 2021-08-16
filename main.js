cfb5db2c0c6caca93ec3d8edc19f337d5ec0d0d6
ttenneb <bgarcia2324@gmail.com>
2021-08-16 13:02:27 -0400

    Added head removal algorithm V1

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let snailRowMap = new Map();
 
 let image = new MarvinImage();
 let image_original;
+let image_original2;
 let mouseDown = false;
 let shiftDown = false;
 let mouse_x = 0;
@@ -96,7 +97,6 @@ function downloadButton(){
     snailMeasuredPoints.forEach((s) => {
        output += s.x1 + ", " + s.y1 + ", " + s.species+  ", " + s.max + '\n';
     });
-    console.log(output);
     download("output.csv", output)
 }
 document.getElementById("show").addEventListener("click", () => {
@@ -152,6 +152,7 @@ function upload() {
             }
         }
         image_original = image.clone();
+        image_original2 = image.clone();
         loaded = true;
         draw();
 
@@ -160,6 +161,7 @@ function upload() {
 document.getElementById("myFile").click();
 function pen(){
     image.fillRect(Math.floor(mouse_x-1), Math.floor(mouse_y-1), 3,3, 0xFF0000FF);
+    image_original.fillRect(Math.floor(mouse_x-1), Math.floor(mouse_y-1), 3,3, 0xFF0000FF);
     working_image.fillRect(Math.floor(mouse_x-1), Math.floor(mouse_y-1), 3,3, 0xFF0000FF);
 }
 function select(){
@@ -265,24 +267,25 @@ function isValidSelection(){
 function crop(){
         if (box_height < 0 && box_width < 0) {
             //TODO do scale before floor
-            Marvin.crop(image, selection,Math.floor(mouse_down_x) + Math.floor(box_width), Math.floor(mouse_down_y) + Math.floor(box_height), -1 * Math.floor(box_width), -1 * Math.floor(box_height));
+            Marvin.crop(image_original, selection,Math.floor(mouse_down_x) + Math.floor(box_width), Math.floor(mouse_down_y) + Math.floor(box_height), -1 * Math.floor(box_width), -1 * Math.floor(box_height));
         } else if (box_height < 0) {
-            Marvin.crop(image, selection,Math.floor(mouse_down_x), Math.floor(mouse_down_y) + Math.floor(box_height), Math.floor(box_width), -1* Math.floor(box_height));
+            Marvin.crop(image_original, selection,Math.floor(mouse_down_x), Math.floor(mouse_down_y) + Math.floor(box_height), Math.floor(box_width), -1* Math.floor(box_height));
         } else if (box_width < 0) {
-            Marvin.crop(image, selection,Math.floor(mouse_down_x) + Math.floor(box_width), Math.floor(mouse_down_y), -1 * Math.floor(box_width), Math.floor(box_height));
+            Marvin.crop(image_original, selection,Math.floor(mouse_down_x) + Math.floor(box_width), Math.floor(mouse_down_y), -1 * Math.floor(box_width), Math.floor(box_height));
         } else {
-            Marvin.crop(image, selection,Math.floor(mouse_down_x), Math.floor(mouse_down_y), Math.floor(box_width), Math.floor(box_height));
+            Marvin.crop(image_original, selection,Math.floor(mouse_down_x), Math.floor(mouse_down_y), Math.floor(box_width), Math.floor(box_height));
         }
 
     process();
 }
 async function process(){
     Marvin.prewitt(selection.clone(), selection);
+    //image = selection;
     let edges = [];
     let edgeMap = new Map()
     for(let i = 0; i < selection.getWidth(); i++){
         for(let j = 0; j < selection.getHeight(); j++){
-            if(selection.getIntComponent0(i,j) > 140 && selection.getIntComponent2(i,j) > 230){
+            if(selection.getIntComponent0(i,j) > 80 && selection.getIntComponent2(i,j) > 230){
                 edgeMap.set(i+j*image.getWidth(), edges.length);
                 edges.push({x:i,y:j, assigned: -1});
             }
@@ -292,7 +295,7 @@ async function process(){
     for(const e of edges){
        if(e.assigned == -1){
            snails[snails.length] = [];
-           await search(e, edgeMap, edges, snails.length-1, snails);
+           search(e, edgeMap, edges, snails.length - 1, snails);
        }
     };
     snails.forEach((edgeList) =>
@@ -303,6 +306,7 @@ async function process(){
         let jmax = 0;
         let current = snailMeasuredPoints.length;
         edgeList.forEach((s) => {
+            image.setIntColor(Math.floor(s.x+mouse_down_x), Math.floor(s.y+mouse_down_y), 0xFFFF0000);
             if (s.x < x.min) {
                 x.min = s.x
             }
@@ -316,11 +320,12 @@ async function process(){
                 y.max = s.y
             }
         });
+        if(edgeList.length > 200)
+            edgeList = calculateOutliers(edgeList);
 
         for (let i = 0; i < edgeList.length; i++) {
             let pointMax = 0;
-
-
+            
             for (let j = i; j < edgeList.length; j++) {
                 let dist = Math.sqrt(Math.pow(edgeList[i].x - edgeList[j].x, 2) + Math.pow(edgeList[i].y - edgeList[j].y, 2));
                 if (dist > pointMax) {
@@ -355,7 +360,7 @@ async function process(){
     for(let i = 0; i < snailMeasuredPoints.length; i++){
         for(let j = i+1; j < snailMeasuredPoints.length; j++){
             if(DoBoxesIntersect(snailMeasuredPoints[i], snailMeasuredPoints[j])){
-                console.log("guh");
+
                 let min = Math.min(snailMeasuredPoints[i].max, snailMeasuredPoints[j].max);
                 if(min == snailMeasuredPoints[i].max){
                     toBeDeleted.push(i);
@@ -366,13 +371,10 @@ async function process(){
             }
         }
     }
-    console.log(snailMeasuredPoints)
-    console.log(toBeDeleted);
+    
     for(let i = toBeDeleted.length-1; i > -1; i--){
         removeSnail(snailMeasuredPoints[toBeDeleted[i]]);
-    }
-    console.log(snailMeasuredPoints)
-    
+    }    
 }
 function search(e, edgeMap, edges, snail, snails) {
     let index = edgeMap.get(e.x+e.y*image.getWidth());
@@ -472,7 +474,7 @@ function drawLine( x1,  y1,  x2,  y2, size, color, image) {
         let slope =  (y2 - y1) / (x2 - x1);
         for (let i = 0; i < x2 - x1; i++) {
             for(let j = 0; j < size; j++) {
-                let round = Math.round(y1 + i * slope);
+                let round = Math.floor(y1 + i * slope);
                 image.setIntColor(x1 + i - j, Math.floor(round), color);
                 image.setIntColor(x1 + i + j, Math.floor(round), color);
                 image.setIntColor(x1 + i, Math.floor(round) - j, color);
@@ -484,7 +486,7 @@ function drawLine( x1,  y1,  x2,  y2, size, color, image) {
         let slope = (x2 - x1) /  (y2 - y1);
         for (let i = 0; i < y2 - y1; i++) {
             for (let j = 0; j < size; j++) {
-                let round = Math.round(x1 + i * slope);
+                let round = Math.floor(x1 + i * slope);
                 image.setIntColor(Math.floor(round), y1 + i, color);
                 image.setIntColor(Math.floor(round), y1 + i - j, color);
                 image.setIntColor(Math.floor(round), y1 + i + j, color);
@@ -509,4 +511,32 @@ function download(filename, text) {
 function DoBoxesIntersect( a, b) {
     return (Math.abs((a.minx + (a.maxx-a.minx)/2) - (b.minx + (b.maxx-b.minx)/2)) * 2 < ((a.maxx-a.minx) + (b.maxx-b.minx))) &&
            (Math.abs((a.miny + (a.maxy-a.miny)/2) - (b.miny + (b.maxy-b.miny)/2)) * 2 < ((a.maxy-a.miny) + (b.maxy-b.miny)));
-  }
+}
+function calculateOutliers(set){
+    console.log(set)
+    let colorSet = [];
+    set.forEach((p) => {
+        let val =  (working_image.getIntComponent0(Math.floor(p.x+mouse_down_x),Math.floor(p.y+mouse_down_y))*1.5+working_image.getIntComponent2(Math.floor(p.x+mouse_down_x),Math.floor(p.y+mouse_down_y))-working_image.getIntComponent1(Math.floor(p.x+mouse_down_x),Math.floor(p.y+mouse_down_y)));
+        let color = {"red":val, "x": p.x+mouse_down_x, "y":p.y+mouse_down_y, "assigned":p.assigned};
+        colorSet.push(color);
+    });
+    colorSet.sort((a,b) => a.red-b.red);
+    let Q1 = colorSet[Math.floor(colorSet.length/4)].red;
+    let Q3 = colorSet[Math.floor(3*colorSet.length/4)].red;
+    let IQR = Q3-Q1;
+    let threshold = Q1+IQR*.3;
+
+    let output = []; 
+    colorSet.forEach((p) => {
+        if(p.red >= threshold){
+            image.setIntColor(Math.floor(p.x),Math.floor(p.y),0xFF00FF00);
+        }else{
+            output.push({
+                "x": p.x-mouse_down_x,
+                "y": p.y-mouse_down_y,
+                "assigned": p.assigned
+            });
+        }
+    });
+    return output;
+}
